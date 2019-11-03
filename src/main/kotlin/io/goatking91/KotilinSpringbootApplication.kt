04b2d377e53e328148a8974c2dd691c8cbdffa14
Selim Yeom <tmdals3021@gmail.com>
2019-11-03 12:21:45 +0900

    kotlin-springboot-1 스프링 부트 프로젝트 생성

---
+++ src/main/kotlin/io/goatking91/KotilinSpringbootApplication.kt
@@ -0,0 +1,11 @@
+package io.goatking91
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotilinSpringbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotilinSpringbootApplication>(*args)
+}
---
+++ src/test/kotlin/io/goatking91/KotilinSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.goatking91
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotilinSpringbootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
