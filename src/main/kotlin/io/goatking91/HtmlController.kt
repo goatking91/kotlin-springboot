d5479917be6ae0038e614e16d8bc9a4d36968a04
Selim Yeom <tmdals3021@gmail.com>
2019-11-03 13:03:00 +0900

    kotlin-springboot-4 등록, 로그인 페이지 개발

--- src/main/kotlin/io/goatking91/HtmlController.kt
+++ src/main/kotlin/io/goatking91/HtmlController.kt
@@ -10,6 +10,19 @@ class HtmlController {
 
     @GetMapping("/")
     fun index(model: Model): String {
+        model.addAttribute("title", "Home")
         return "index"
     }
+
+    @GetMapping("/{formType}")
+    fun htmlForm(model: Model, @PathVariable formType: String): String {
+        var response: String = ""
+        when (formType) {
+            "sign" -> response = "sign"
+            "login" -> response = "login"
+        }
+        model.addAttribute("title", response)
+        return response
+    }
+
 }
